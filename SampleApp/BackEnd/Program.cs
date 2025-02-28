352a655879bf3275ad834daccacd7c1ce882c7a2
James Montemagno <james.montemagno@gmail.com>
2025-02-28 16:33:02 +0000

    refactor: simplify namespace declarations and improve code formatting

--- SampleApp/BackEnd/Program.cs
+++ SampleApp/BackEnd/Program.cs
@@ -1,3 +1,5 @@
+using Scalar.AspNetCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
--- SampleApp/FrontEnd/Data/WeatherForecastClient.cs
+++ SampleApp/FrontEnd/Data/WeatherForecastClient.cs
@@ -1,17 +1,16 @@
-namespace FrontEnd.Data
+namespace FrontEnd.Data;
+
+public class WeatherForecastClient
 {
-    public class WeatherForecastClient
+    private HttpClient _httpClient;
+    private ILogger<WeatherForecastClient> _logger;
+
+    public WeatherForecastClient(HttpClient httpClient, ILogger<WeatherForecastClient> logger)
     {
-        private HttpClient _httpClient;
-        private ILogger<WeatherForecastClient> _logger;
-
-        public WeatherForecastClient(HttpClient httpClient, ILogger<WeatherForecastClient> logger)
-        {
-            _httpClient = httpClient;
-            _logger = logger;
-        }
-
-        public async Task<WeatherForecast[]> GetForecastAsync(DateTime? startDate)
-            => await _httpClient.GetFromJsonAsync<WeatherForecast[]>($"WeatherForecast?startDate={startDate}");
+        _httpClient = httpClient;
+        _logger = logger;
     }
+
+    public async Task<WeatherForecast[]> GetForecastAsync(DateTime? startDate)
+        => await _httpClient.GetFromJsonAsync<WeatherForecast[]>($"WeatherForecast?startDate={startDate}") ?? [];
 }
